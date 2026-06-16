09c76bd8090272a157bb76ccfd212a869d967f51
DanielOrtiz08 <ddov0811@gmail.com>
2026-06-16 12:55:03 -0500

    first commit

---
+++ api-gateway/src/test/java/com/daniel/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
